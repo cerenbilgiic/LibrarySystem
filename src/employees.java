3645377231e7b59db9b04ca48dda063d12063461
cerenbilgiic <GitHub-ceren1966@icloud.com>
2026-04-07 22:04:47 +0300

    .

--- src/employees.java
+++ src/employees.java
@@ -4,12 +4,12 @@ public class employees extends users {
     public employees(int id, String first_name , String last_name , String username , String password , String role , LocalDate created_at ){
         //user classının özelliklerinin girişi yapılıyor.
         super(id , first_name , last_name , username , password, role , created_at );
-        role= "Kütüphane Çalışanı";
+        role= "KÜTÜPHANE ÇALIŞANI";
     }
 
     @Override
     public boolean login() {
-        System.out.println(getFullName() + " isimli üye başarıyla giriş yaptı.");
+        System.out.println(getFullName() + " isimli çalışan başarıyla giriş yaptı.");
         return true;
     }
 
